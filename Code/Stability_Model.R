121e2fa2bed1bde9bbb324c22489224377bf11ca
Nayan Chawla <nnchawla@go.olemiss.edu>
2018-10-22 10:36:25 -0500

    working sd heatmap

--- Code/Stability_Model.R
+++ Code/Stability_Model.R
@@ -14,22 +14,28 @@ CvNs <- function(S, C, step) {
   communities <- rep(list(rep(list(vector("list", matrixSize)), matrixSize)), replicates)
   persistences <- rep(list(rep(list(vector("list", matrixSize)), matrixSize)), replicates)
   means <- rep(list(rep(list(0), matrixSize)), matrixSize)
-  stdDevs <- rep(list(rep(list(vector("list", replicates)), matrixSize)), matrixSize)
+  stdDevs <- rep(list(rep(list(vector("integer", replicates)), matrixSize)), matrixSize)
 
   for (CvN in 1:replicates) {
     C_step <- step
-    S_step <- S*step
+    S_step <- S/(C/step)
     
     for(rowC in 1:matrixSize) {
-
+      
+      # if (C_step > 0.5*(1-(1/S))) {
+      #     C_step <- C_step + step
+      #     next
+      # }
+      
       for(colN in 1:matrixSize) {
-
+        #print(c(C_step, S_step))
+        
         L <- round(S_step^2*C_step)  ## calculate number of links from S and C
         
         # skipping community generation because formula is less than 0 which throws error
         if (((S_step^2 - S_step)/2 - L) < 0) {
           #print(c(S_step, C_step, "error"))
-          S_step <- S_step + step * S
+          S_step <- S_step + S/(C/step)
           next
         }
         
@@ -67,50 +73,55 @@ CvNs <- function(S, C, step) {
         
         out <- n.integrate(time, init.x, model = mougi_model)
         communities[[CvN]][[rowC]][[colN]] <- out
-        persistences[[CvN]][[rowC]][[colN]] <- mean(out[nrow(out),2:n+1] > 10^-5, )
+        persistences[[CvN]][[rowC]][[colN]] <- mean(out[nrow(out),2:n+1] > 10^-5)
         
-        S_step <- S_step + step * S
+        S_step <- S_step + S/(C/step)
       }
       
-      S_step <- S*step
+      S_step <- S/(C/step)
       C_step <- C_step + step
       
     }
   
   }
   
-  #setting up means and stdDevs
   for(CvN in 1:replicates) {
       
     for(rowC in 1:matrixSize) {
       
       for(colN in 1:matrixSize) {
         
-        #print(c(CvN, rowC, rowN))
+        #print(c(CvN, rowC, colN))
         means[[rowC]][[colN]] <- means[[rowC]][[colN]] + persistences[[CvN]][[rowC]][[colN]]
-        stdDevs[[rowC]][[colN]][[CvN]] <- persistences[[CvN]][[rowC]][[colN]]
-        
+        if(!is.null(persistences[[CvN]][[rowC]][[colN]]))
+          stdDevs[[rowC]][[colN]][[CvN]] <- persistences[[CvN]][[rowC]][[colN]]
       }
     }
   }
   
   for(rowC in 1:matrixSize) {
     for(colN in 1:matrixSize) {
-      means[[rowC]][[colN]] <- means[[rowC]][[colN]]/10.0
       stdDevs[[rowC]][[colN]] <- sd(stdDevs[[rowC]][[colN]])
+      #print(c(rowC, colN, stdDevs[[rowC]][[colN]]))
     }
   }
   
-  #stdDevs[[rowC]][[colN]] <- sd(out[nrow(out),2:n+1] > 10^-5)
+  speciesLabels <- c(seq((S/(C/step)), S, (S/(C/step))))
+  connectanceLabels <- c(seq(step, C, step))
+  stdDevs <- as.data.frame(matrix(unlist(stdDevs), nrow=length(unlist(stdDevs[1]))))
+  rownames(stdDevs) <- connectanceLabels
+  colnames(stdDevs) <- speciesLabels
   
   matricies <- list("communities" = communities, "persistences" = persistences, "mean" = means, "stdDev" = stdDevs)
   return(matricies)
 
 }
 
-plotCommunity <- function(targetMatrix, row_num, col_num) {
-  
-  ggplot(data <- melt(targetMatrix[[row_num]][[col_num]], id.vars = "time")) +
-    geom_point(mapping = aes(x = time, y = value, color = variable))
-  
+plotGraph <- function(dataFrame, graphType) {
+    ggplot(melt(as.matrix(dataFrame), id.vars = c("X1", "X2", "value"))) +
+    scale_fill_gradient(low = "steelblue", high = "white") +
+    ylab("Connectance") +
+    xlab("Species") +
+    geom_tile(mapping = aes(x = X2, y = X1, fill = value)) +
+    labs(fill = graphType)
 }
