282b2f120d077fe8774beb54f4a50ee6fd27e259
Nayan Chawla <nnchawla@go.olemiss.edu>
2019-09-26 02:18:50 -0500

    forge

--- Code/VectorGraphs.R
+++ Code/VectorGraphs.R
@@ -17,37 +17,99 @@ fixDS <- function(n){
   return(dsCopy)
 }
 
-pltTbFrames <- function(mats, single = FALSE, connectance = 0.1, richness = 20){
-  if(!single) {
-    plt <- ggplot(mats, aes(x=Step, y=value, color = factor(R)))
-    plt <- plt + facet_wrap(vars(factor(C)))
-    plt <- plt + geom_point() + geom_line() + theme(legend.position = "None") 
+mergeDatasets <- function(containers){
+  dfs <- list()
+  for (i in 1:length(containers)) {
+    df <- containers[[i]]$Steps
+    df <- add_column(df, Mutualism = rep(containers[[i]]$Parms[[4]], nrow(df)),
+                     Exploitation = rep(containers[[i]]$Parms[[5]], nrow(df)),
+                     Competition = rep(containers[[i]]$Parms[[6]], nrow(df)),
+                     .before = 1)
+    dfs[[i]] <- df
   }
-  else{
-    mats <- mats %>% filter(C == connectance & N == richness)
-    R <- c()
-    val <- c()
-    step <- c()
-    for(i in 1:nrow(mats)){
-      R <- c(R, rep(mats$R[i], mats$eqSteps[i]))
-      val <- c(val, rep(mats$value[i], mats$eqSteps[i]))
-    }
-    
-    for(i in 1:length(rle(R)$values)){
-      step <- c(step, seq(1, rle(R)$lengths[[i]]))
-    }
-    
-    browser()
-    mats <- as.tibble(data.frame(R = R, value = val, Step = step))
-    plt <- ggplot(mats, aes(x=Step, y=value, color=factor(R)))
-    plt <- plt + geom_line(size = 1.25) + xlab("Step Number") + ylab("Nisle")
-    plt <- plt + ggtitle(paste("Connectance", connectance, "Species #", richness))
-  }
-  return(plt)
+  dfs <- bind_rows(dfs)
+  dfs$eqSteps[is.na(dfs$eqSteps)] <- 1
+  return(dfs)
 }
 
-multiGraph <- function(multiMat, containers){
-  massGraphs <- list()
+probZ <- function(){
+  probs <- list()
+  count <- 1
+  for(p.m in seq(0, 1, 0.1)){
+    for(p.e in seq(0, 1, 0.1)) {
+      p.c = 1.0 - (p.m + p.e)
+      if(p.m+p.e+p.c == 1 && p.c > 0){
+        probs[[count]] <- c(p.m, p.e, 1-(p.m+p.e))
+        count <- count + 1
+      }
+    }
+  }
+  return(probs)
+}
+
+forge <- function(dfs) {
+  probs <- probZ()
+  tables <- vector(mode = "list", length = 5500)
+  count <- 1
+  
+  for(i in seq(0.1, 1, 0.1)) {
+    for(j in seq(20, 200, 20)) {
+      for(k in 1:length(probs)) {
+        tables[[count]] <- meanPath(dfs, connectance = i, richness = j, Mut = probs[[k]][1], Exp = probs[[k]][2], Comp = probs[[k]][3])
+        count <- count + 1
+        print(c(i, j, k))
+      }
+    }
+  }
+  return(tables)
+}
+
+meanPath <- function(mats, connectance = 0.1, richness = 20, Mut, Exp, Comp){
+  mats <- mats %>% filter(C == connectance & N == richness & Mutualism == Mut & Exploitation == Exp & Competition == Comp)
+  mats$eqSteps[is.na(mats$eqSteps)] = 1
+  R <- c()
+  val <- c()
+  step <- c()
+  maxVal <- 0
+  for(i in 1:nrow(mats)){
+    R <- c(R, rep(mats$R[i], mats$eqSteps[i]))
+    val <- c(val, rep(mats$value[i], mats$eqSteps[i]))
+  }
+  
+  for(i in 1:length(rle(R)$values)){
+    x <- seq(1, rle(R)$lengths[[i]])
+    if(length(x) > maxVal) {
+      maxVal <- length(x)
+    }
+    step <- c(step, x)
+  }
+  
+  mats <- as.tibble(data.frame(R = R, value = val, Step = step))
+  values <- vector(mode="list", length=maxVal)
+  steps <- 1:maxVal
+  muts <- rep(Mut, maxVal)
+  exps <- rep(Exp, maxVal)
+  coms <- rep(Comp, maxVal)
+  conns <- rep(connectance, maxVal)
+  speciesN <- rep(richness, maxVal)
+  
+  for(i in steps){
+    tmpMat <- mats %>% filter(Step == i)
+    values[[i]] <- mean(tmpMat$value)
+  }
+  
+  values <- unlist(values)
+  mats <- as.tibble(data.frame(M=muts, E=exps, Comp=coms, C=conns, N=speciesN, Step=steps, Persistence=values))
+  return(mats)
+  # plt <- ggplot(mats, aes(x=Step, y=N))
+  # plt <- plt + geom_line(size = 1.25) + xlab("Step Number") + ylab("Nisle")
+  # plt <- plt + ggtitle(paste("Connectance", connectance, "Species #", richness))
+  # 
+  # return(plt)
+}
+  
+  multiGraph <- function(multiMat, containers){
+    massGraphs <- list()
   tGraphs <- list()
   pGraphs <- list()
   count <- 1
@@ -80,7 +142,7 @@ simplexGraph <- function(containers, connectance, numSpecies) {
     x <- x$TimeMatrix %>% filter(C == cn & N == ns)
     
     #return either mean of final persistence or of total steps
-    return(mean(x$p100))
+    return(mean(x$t100))
   }
   
   pm <- c()
@@ -115,7 +177,7 @@ simplexGraph <- function(containers, connectance, numSpecies) {
   
   palette <- c( "#FF9933", "#002C54", "#3375B2", "#CCDDEC", "#BFBFBF", "#000000")
   simplex <- ggtern(data=df, aes(E, C, M)) + 
-    geom_hex_tern(aes(value=N), color="black", alpha=1)
+    geom_hex_tern(aes(value=log(N)), color="black", alpha=1)
   simplex <- simplex + scale_fill_gradient(name="Scale", low=palette[2], high=palette[4])
   
   #simplex <- simplex + Tarrowlab("Competition") +
@@ -153,8 +215,8 @@ lineArrange <- function(containers, N) {
 pdfSimplex <- function(containers) {
   N <- seq(20, 200, 20)
   for(i in 1:length(N)) {
-    #pdf(paste("p100-", N[[i]], ".pdf", sep=""))
-    #print(simplexArrange(containers, N[[i]]))
+    pdf(paste("t100-log-", N[[i]], ".pdf", sep=""))
+    print(simplexArrange(containers, N[[i]]))
     dev.off()
   }
 }
